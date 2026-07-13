9f277e4f065183a6c6dd33a8468f89b4399daedc
CoLe <collinsolads@gmail.com>
2026-07-13 12:01:21 +0100

    Open CTA links in a new tab

--- components/landing/cta/cta.jsx
+++ components/landing/cta/cta.jsx
@@ -6,6 +6,8 @@ export function PrimaryCta({ children }) {
   return (
     <Link
       href="https://app.skillduel.app/auth?next=%2F"
+      target="_blank"
+      rel="noopener noreferrer"
       className={styles.primary}
     >
       {children}
@@ -18,6 +20,8 @@ export function SecondaryCta({ children, icon }) {
   return (
     <Link
       href="https://app.skillduel.app/duel/topic"
+      target="_blank"
+      rel="noopener noreferrer"
       className={styles.secondary}
     >
       {icon}
